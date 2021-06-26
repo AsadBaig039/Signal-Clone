e89f4b9d0ea5589ac4e15d0baf2a55e04ac318bc
Asad Baig <36822413+AsadBaig039@users.noreply.github.com>
2021-06-26 16:22:30 +0500

    Chat collection created

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,6 +1,13 @@
 import React, { useState, useLayoutEffect } from "react";
-import { TouchableOpacity } from "react-native";
-import { View, StyleSheet, Text } from "react-native";
+import {
+  TouchableOpacity,
+  SafeAreaView,
+  Platform,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { View, StyleSheet, Text, TextInput } from "react-native";
 import { Avatar } from "react-native-elements";
 import {
   AntDesign,
@@ -8,8 +15,14 @@ import {
   FontAwesome,
   Ionicons,
 } from "@expo/vector-icons";
+import { ScrollView } from "react-native";
+import { Keyboard } from "react-native";
+import * as firebase from "firebase";
+import { db, auth } from "../firebase";
 
 function ChatScreen({ navigation, route }) {
+  const [input, setInput] = useState();
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: () => (
@@ -48,16 +61,71 @@ function ChatScreen({ navigation, route }) {
         </View>
       ),
     });
-  }, []);
+  }, [navigation]);
+
+  const sendMessage = () => {
+    Keyboard.dismiss();
+    db.collection("chats").doc(route.params.id).collection("messages").add({
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      message: input,
+      displayName: auth.currentUser.displayName,
+      email: auth.currentUser.email,
+      photoURL: auth.currentUser.photoURL,
+    });
+  };
   return (
-    <View style={styles.container}>
-      <Text>{route.params.chatName}</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
+      <StatusBar style="light" />
+      <KeyboardAvoidingView
+        // behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+        keyboardVerticalOffset={90}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <>
+            <ScrollView></ScrollView>
+            {/* chat goes here */}
+
+            <View style={styles.footer}>
+              <TextInput
+                placeholder="Signal Message"
+                style={styles.textInput}
+                value={input}
+                onChangeText={(text) => setInput(text)}
+                onSubmitEditing={sendMessage}
+              />
+              <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+                <Ionicons name="send" size={24} color="#2B68E6" />
+              </TouchableOpacity>
+            </View>
+          </>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
+  footer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    padding: 15,
+  },
+  textInput: {
+    bottom: 0,
+    height: 40,
+    flex: 1,
+    marginRight: 15,
+    backgroundColor: "#ECECEC",
+    padding: 10,
+    color: "grey",
+    borderRadius: 30,
+    marginBottom: 5,
+  },
 });
 
 export default ChatScreen;
