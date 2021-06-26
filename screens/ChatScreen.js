6da9160d5425f795b904e9e7af0dc1123b04d629
Asad Baig <36822413+AsadBaig039@users.noreply.github.com>
2021-06-26 17:25:55 +0500

    Chat Implemented for both send and receiver

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useLayoutEffect } from "react";
+import React, { useState, useLayoutEffect, useEffect } from "react";
 import {
   TouchableOpacity,
   SafeAreaView,
@@ -22,6 +22,7 @@ import { db, auth } from "../firebase";
 
 function ChatScreen({ navigation, route }) {
   const [input, setInput] = useState();
+  const [messages, setMessages] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -72,7 +73,26 @@ function ChatScreen({ navigation, route }) {
       email: auth.currentUser.email,
       photoURL: auth.currentUser.photoURL,
     });
+
+    setInput("");
   };
+
+  useLayoutEffect(() => {
+    const unsubscribe = db
+      .collection("chats")
+      .doc(route.params.id)
+      .collection("messages")
+      .orderBy("timestamp", "asc")
+      .onSnapshot((snapshot) =>
+        setMessages(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+    return unsubscribe;
+  }, [route]);
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "white" }}>
       <StatusBar style="light" />
@@ -83,8 +103,46 @@ function ChatScreen({ navigation, route }) {
       >
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <>
-            <ScrollView></ScrollView>
-            {/* chat goes here */}
+            <ScrollView contentContainerStyle={{ paddingTop: 15 }}>
+              {messages.map(({ id, data }) =>
+                data.email === auth.currentUser.email ? (
+                  <View key={id} style={styles.receiver}>
+                    <Avatar
+                      source={{ uri: data.photoURL }}
+                      size={30}
+                      bottom={-15}
+                      right={-5}
+                      position="absolute"
+                      rounded
+                      containerStyle={{
+                        position: "absolute",
+                        bottom: -15,
+                        right: -5,
+                      }}
+                    />
+                    <Text style={styles.receiverText}>{data.message}</Text>
+                  </View>
+                ) : (
+                  <View key={id} style={styles.sender}>
+                    <Avatar
+                      source={{ uri: data.photoURL }}
+                      size={30}
+                      bottom={-15}
+                      right={-5}
+                      position="absolute"
+                      rounded
+                      containerStyle={{
+                        position: "absolute",
+                        bottom: -15,
+                        right: -5,
+                      }}
+                    />
+                    <Text style={styles.senderText}>{data.message}</Text>
+                    <Text style={styles.senderName}>{data.displayName}</Text>
+                  </View>
+                )
+              )}
+            </ScrollView>
 
             <View style={styles.footer}>
               <TextInput
@@ -108,6 +166,42 @@ function ChatScreen({ navigation, route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  receiver: {
+    padding: 15,
+    backgroundColor: "#2B68E6",
+    alignSelf: "flex-end",
+    borderRadius: 20,
+    marginRight: 15,
+    marginBottom: 20,
+    maxWidth: "80%",
+    position: "relative",
+  },
+  sender: {
+    padding: 15,
+    backgroundColor: "#ECECEC",
+    alignSelf: "flex-start",
+    borderRadius: 20,
+    margin: 15,
+    maxWidth: "80%",
+    position: "relative",
+  },
+  senderName: {
+    left: 10,
+    paddingRight: 10,
+    fontSize: 10,
+    color: "black",
+  },
+  senderText: {
+    color: "black",
+    fontWeight: "500",
+    marginLeft: 10,
+    marginBottom: 5,
+  },
+  receiverText: {
+    color: "white",
+    fontWeight: "500",
+    marginLeft: 10,
   },
   footer: {
     flexDirection: "row",
